f69172fe18bbc3fb227764c539e782e31cd162c6
Aaron Madved <aaronmadved@gmail.com>
2014-07-09 18:14:15 -0400

    Recent Changes before adding the Send Button

--- Ribbit/build/generated/source/r/debug/com/teamtreehouse/ribbit/R.java
+++ Ribbit/build/generated/source/r/debug/com/teamtreehouse/ribbit/R.java
@@ -38,14 +38,16 @@ public final class R {
         public static final int activity_edit_friends=0x7f030000;
         public static final int activity_login=0x7f030001;
         public static final int activity_main=0x7f030002;
-        public static final int activity_sign_up=0x7f030003;
-        public static final int fragment_friends=0x7f030004;
-        public static final int fragment_inbox=0x7f030005;
+        public static final int activity_recipients=0x7f030003;
+        public static final int activity_sign_up=0x7f030004;
+        public static final int fragment_friends=0x7f030005;
+        public static final int fragment_inbox=0x7f030006;
     }
     public static final class menu {
         public static final int login=0x7f080000;
         public static final int main=0x7f080001;
-        public static final int sign_up=0x7f080002;
+        public static final int recipients=0x7f080002;
+        public static final int sign_up=0x7f080003;
     }
     public static final class string {
         public static final int action_settings=0x7f060000;
@@ -53,30 +55,32 @@ public final class R {
         public static final int email_hint=0x7f060002;
         public static final int empty_friends_label=0x7f060003;
         public static final int empty_inbox_label=0x7f060004;
-        public static final int error_file_size_too_large=0x7f060005;
-        public static final int error_opening_file=0x7f060006;
-        public static final int error_title=0x7f060007;
-        public static final int general_error=0x7f060008;
-        public static final int hello_world=0x7f060009;
-        public static final int login_button_label=0x7f06000a;
-        public static final int login_error_message=0x7f06000b;
-        public static final int login_error_title=0x7f06000c;
-        public static final int menu_camera_label=0x7f06000d;
-        public static final int menu_edit_friends_label=0x7f06000e;
-        public static final int menu_logout_label=0x7f06000f;
-        public static final int password_hint=0x7f060010;
-        public static final int sign_up_button_label=0x7f060011;
-        public static final int sign_up_text=0x7f060012;
-        public static final int signup_error_message=0x7f060013;
-        public static final int signup_error_title=0x7f060014;
-        public static final int storage_error_message=0x7f060015;
-        public static final int title_activity_edit_friends=0x7f060016;
-        public static final int title_activity_login=0x7f060017;
-        public static final int title_activity_sign_up=0x7f060018;
-        public static final int title_section1=0x7f060019;
-        public static final int title_section2=0x7f06001a;
-        public static final int username_hint=0x7f06001b;
-        public static final int video_file_size_warning=0x7f06001c;
+        public static final int empty_recipients_list_message=0x7f060005;
+        public static final int error_file_size_too_large=0x7f060006;
+        public static final int error_opening_file=0x7f060007;
+        public static final int error_title=0x7f060008;
+        public static final int general_error=0x7f060009;
+        public static final int hello_world=0x7f06000a;
+        public static final int login_button_label=0x7f06000b;
+        public static final int login_error_message=0x7f06000c;
+        public static final int login_error_title=0x7f06000d;
+        public static final int menu_camera_label=0x7f06000e;
+        public static final int menu_edit_friends_label=0x7f06000f;
+        public static final int menu_logout_label=0x7f060010;
+        public static final int password_hint=0x7f060011;
+        public static final int sign_up_button_label=0x7f060012;
+        public static final int sign_up_text=0x7f060013;
+        public static final int signup_error_message=0x7f060014;
+        public static final int signup_error_title=0x7f060015;
+        public static final int storage_error_message=0x7f060016;
+        public static final int title_activity_edit_friends=0x7f060017;
+        public static final int title_activity_login=0x7f060018;
+        public static final int title_activity_recipients=0x7f060019;
+        public static final int title_activity_sign_up=0x7f06001a;
+        public static final int title_section1=0x7f06001b;
+        public static final int title_section2=0x7f06001c;
+        public static final int username_hint=0x7f06001d;
+        public static final int video_file_size_warning=0x7f06001e;
     }
     public static final class style {
         /** 
