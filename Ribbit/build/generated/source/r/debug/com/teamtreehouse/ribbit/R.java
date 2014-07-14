bb09b5d1f9490b774093e09d80a8986a500d67af
Aaron Madved <aaronmadved@gmail.com>
2014-07-14 10:28:41 -0400

    Created a Custom List Adapter for Messages

--- Ribbit/build/generated/source/r/debug/com/teamtreehouse/ribbit/R.java
+++ Ribbit/build/generated/source/r/debug/com/teamtreehouse/ribbit/R.java
@@ -16,20 +16,28 @@ public final class R {
     public static final class dimen {
         public static final int activity_horizontal_margin=0x7f050000;
         public static final int activity_vertical_margin=0x7f050001;
+        public static final int message_list_item_vertical_margin=0x7f050002;
     }
     public static final class drawable {
         public static final int ic_action_camera=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int ic_action_picture=0x7f020001;
+        public static final int ic_action_play_over_video=0x7f020002;
+        public static final int ic_action_send_now=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
     }
     public static final class id {
-        public static final int action_camera=0x7f090009;
-        public static final int action_edit_friends=0x7f090008;
-        public static final int action_logout=0x7f09000a;
-        public static final int action_settings=0x7f090007;
+        public static final int action_camera=0x7f09000d;
+        public static final int action_edit_friends=0x7f09000b;
+        public static final int action_logout=0x7f09000c;
+        public static final int action_send=0x7f09000e;
         public static final int emailField=0x7f090005;
+        public static final int imageView=0x7f090007;
         public static final int loginButton=0x7f090002;
+        public static final int messageIcon=0x7f090009;
         public static final int pager=0x7f090004;
         public static final int passwordField=0x7f090001;
+        public static final int section_label=0x7f090008;
+        public static final int senderLabel=0x7f09000a;
         public static final int signUpText=0x7f090003;
         public static final int signupButton=0x7f090006;
         public static final int usernameField=0x7f090000;
@@ -40,14 +48,15 @@ public final class R {
         public static final int activity_main=0x7f030002;
         public static final int activity_recipients=0x7f030003;
         public static final int activity_sign_up=0x7f030004;
-        public static final int fragment_friends=0x7f030005;
-        public static final int fragment_inbox=0x7f030006;
+        public static final int activity_view_image=0x7f030005;
+        public static final int fragment_friends=0x7f030006;
+        public static final int fragment_inbox=0x7f030007;
+        public static final int fragment_main_dummy=0x7f030008;
+        public static final int message_item=0x7f030009;
     }
     public static final class menu {
-        public static final int login=0x7f080000;
-        public static final int main=0x7f080001;
-        public static final int recipients=0x7f080002;
-        public static final int sign_up=0x7f080003;
+        public static final int main=0x7f080000;
+        public static final int recipients=0x7f080001;
     }
     public static final class string {
         public static final int action_settings=0x7f060000;
@@ -56,31 +65,39 @@ public final class R {
         public static final int empty_friends_label=0x7f060003;
         public static final int empty_inbox_label=0x7f060004;
         public static final int empty_recipients_list_message=0x7f060005;
-        public static final int error_file_size_too_large=0x7f060006;
-        public static final int error_opening_file=0x7f060007;
-        public static final int error_title=0x7f060008;
-        public static final int general_error=0x7f060009;
-        public static final int hello_world=0x7f06000a;
-        public static final int login_button_label=0x7f06000b;
-        public static final int login_error_message=0x7f06000c;
-        public static final int login_error_title=0x7f06000d;
-        public static final int menu_camera_label=0x7f06000e;
-        public static final int menu_edit_friends_label=0x7f06000f;
-        public static final int menu_logout_label=0x7f060010;
-        public static final int password_hint=0x7f060011;
-        public static final int sign_up_button_label=0x7f060012;
-        public static final int sign_up_text=0x7f060013;
-        public static final int signup_error_message=0x7f060014;
-        public static final int signup_error_title=0x7f060015;
-        public static final int storage_error_message=0x7f060016;
-        public static final int title_activity_edit_friends=0x7f060017;
-        public static final int title_activity_login=0x7f060018;
-        public static final int title_activity_recipients=0x7f060019;
-        public static final int title_activity_sign_up=0x7f06001a;
-        public static final int title_section1=0x7f06001b;
-        public static final int title_section2=0x7f06001c;
-        public static final int username_hint=0x7f06001d;
-        public static final int video_file_size_warning=0x7f06001e;
+        public static final int error_external_storage=0x7f060006;
+        public static final int error_file_size_too_large=0x7f060007;
+        public static final int error_file_too_large=0x7f060008;
+        public static final int error_opening_file=0x7f060009;
+        public static final int error_selecting_file=0x7f06000a;
+        public static final int error_selecting_file_title=0x7f06000b;
+        public static final int error_sending_message=0x7f06000c;
+        public static final int error_title=0x7f06000d;
+        public static final int general_error=0x7f06000e;
+        public static final int hello_world=0x7f06000f;
+        public static final int login_button_label=0x7f060010;
+        public static final int login_error_message=0x7f060011;
+        public static final int login_error_title=0x7f060012;
+        public static final int menu_camera_label=0x7f060013;
+        public static final int menu_edit_friends_label=0x7f060014;
+        public static final int menu_logout_label=0x7f060015;
+        public static final int menu_send_label=0x7f060016;
+        public static final int password_hint=0x7f060017;
+        public static final int sign_up_button_label=0x7f060018;
+        public static final int sign_up_text=0x7f060019;
+        public static final int signup_error_message=0x7f06001a;
+        public static final int signup_error_title=0x7f06001b;
+        public static final int success_message=0x7f06001c;
+        public static final int title_activity_edit_friends=0x7f06001d;
+        public static final int title_activity_login=0x7f06001e;
+        public static final int title_activity_recipients=0x7f06001f;
+        public static final int title_activity_sign_up=0x7f060020;
+        public static final int title_activity_view_image=0x7f060021;
+        public static final int title_section1=0x7f060022;
+        public static final int title_section2=0x7f060023;
+        public static final int username_hint=0x7f060024;
+        public static final int video_file_size_warning=0x7f060025;
+        public static final int video_size_limit_warning=0x7f060026;
     }
     public static final class style {
         /** 
--- Ribbit/src/com/teamtreehouse/ribbit/EditFriendsActivity.java
+++ Ribbit/src/com/teamtreehouse/ribbit/EditFriendsActivity.java
@@ -1,10 +1,13 @@
 package com.teamtreehouse.ribbit;
 
+import java.util.List;
+
 import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
@@ -18,16 +21,14 @@ import com.parse.ParseRelation;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
-import java.util.List;
-
 public class EditFriendsActivity extends ListActivity {
+	
+	protected ParseRelation<ParseUser> mFriendsRelation;
+	protected ParseUser mCurrentUser;
 	
 	public static final String TAG = EditFriendsActivity.class.getSimpleName();
 	
 	protected List<ParseUser> mUsers;
-    protected ParseRelation<ParseUser> mFriendsRelation;
-    protected ParseUser mCurrentUser;
-
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -36,19 +37,17 @@ public class EditFriendsActivity extends ListActivity {
 		setContentView(R.layout.activity_edit_friends);
 		// Show the Up button in the action bar.
 		setupActionBar();
-
-
-        getListView().setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+		
+		getListView().setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
 	}
 	
 	@Override
 	protected void onResume() {
 		super.onResume();
-
-        mCurrentUser = ParseUser.getCurrentUser();
-        mFriendsRelation = mCurrentUser.getRelation(ParseConstants.KEY_FRIENDS_RELATION);
-
-
+		
+		mCurrentUser = ParseUser.getCurrentUser();
+		mFriendsRelation = mCurrentUser.getRelation(ParseConstants.KEY_FRIENDS_RELATION);
+		
 		setProgressBarIndeterminateVisibility(true);
 		
 		ParseQuery<ParseUser> query = ParseUser.getQuery();
@@ -73,8 +72,8 @@ public class EditFriendsActivity extends ListActivity {
 							android.R.layout.simple_list_item_checked,
 							usernames);
 					setListAdapter(adapter);
-
-                    addFriendCheckmarks();
+					
+					addFriendCheckmarks();
 				}
 				else {
 					Log.e(TAG, e.getMessage());
@@ -115,51 +114,59 @@ public class EditFriendsActivity extends ListActivity {
 		return super.onOptionsItemSelected(item);
 	}
 
-    @Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
-        super.onListItemClick(l, v, position, id);
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		super.onListItemClick(l, v, position, id);
+		
+		if (getListView().isItemChecked(position)) {
+			// add the friend
+			mFriendsRelation.add(mUsers.get(position));
+		}
+		else {
+			// remove the friend
+			mFriendsRelation.remove(mUsers.get(position));
+		}
 
-        // Check it's different in "Adding Checkmarks When the List is Loaded" than how the previous ended
-        if (getListView().isItemChecked(position)) {
-            // add friend if checked
-            mFriendsRelation.add(mUsers.get(position));
-        }
-        else {
-            // remove friend
-            mFriendsRelation.remove(mUsers.get(position));
-        }
-        mCurrentUser.saveInBackground(new SaveCallback() {
-            @Override
-            public void done(ParseException e) {
-                if (e != null) {
-                    Log.e(TAG, e.getMessage());
-                }
-            }
-        });
-
-    }
-
-    private void addFriendCheckmarks() {
-        mFriendsRelation.getQuery().findInBackground(new FindCallback<ParseUser>() {
-            @Override
-            public void done(List<ParseUser> friends, ParseException e) {
-                if (e == null) {
-                    // list returned - look for a match
-                    for (int i = 0; i < mUsers.size(); i++) {
-                        ParseUser user = mUsers.get(i);
-
-                        for (ParseUser friend : friends) {
-                            if (friend.getObjectId().equals(user.getObjectId())) {
-                                getListView().setItemChecked(i, true);
-                            }
-                        }
-                    }
-                }
-                else {
-                    Log.e(TAG, e.getMessage());
-                }
-            }
-        });
-    }
-
+		mCurrentUser.saveInBackground(new SaveCallback() {
+			@Override
+			public void done(ParseException e) {
+				if (e != null) {
+					Log.e(TAG, e.getMessage());
+				}
+			}
+		});
+	}
+	
+	private void addFriendCheckmarks() {
+		mFriendsRelation.getQuery().findInBackground(new FindCallback<ParseUser>() {
+			@Override
+			public void done(List<ParseUser> friends, ParseException e) {
+				if (e == null) {
+					// list returned - look for a match
+					for (int i = 0; i < mUsers.size(); i++) {
+						ParseUser user = mUsers.get(i);
+						
+						for (ParseUser friend : friends) {
+							if (friend.getObjectId().equals(user.getObjectId())) {
+								getListView().setItemChecked(i, true);
+							}
+						}
+					}
+				}
+				else {
+					Log.e(TAG, e.getMessage());
+				}
+			}
+		});
+	}
 }
+
+
+
+
+
+
+
+
+
+
